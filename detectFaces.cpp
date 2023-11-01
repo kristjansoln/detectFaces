4849c55b777d9c9a2f8a1d03b9177abee4c5264a
Kristjan <kristjan.maysoln@gmail.com>
2023-11-01 17:23:24 +0100

    Add support for image lists
    Provided input file can now be a text file containing a list of images.
    Expected format is 2002/08/11/big/img_591, without file extensions and
    correct top directory.

--- detectFaces.cpp
+++ detectFaces.cpp
@@ -1,8 +1,8 @@
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 using namespace cv;
@@ -15,12 +15,13 @@ int main(int argc, const char **argv)
     // Parse command line input
     CommandLineParser parser(argc, argv,
                              "{help h||Display this message and exit.}"
-                             "{@image|<none>|Path to image file.}"
+                             "{@inputFilePath|<none>|Path to an image or a txt file containing a list of images.}"
                              "{@cascade|cascades/haarcascade_frontalface_alt.xml|Path to face cascade. Defaults to frontalface_alt.xml}"
+                             "{imageTopDir i|.|Top image directory, will be prepended to the provided image path. Do not add a trailing forward slash.}"
                              "{scale s|1.1|Scale factor specifies how much the image size is reduced at each image scale during detection.}"
                              "{neighbors n|3|Min. neighbors specifies how many neighbors each candidate rectangle should have to retain itself for the next stage.}"
                              "{display d||Display detections.}");
-    parser.about("\nThis program detects faces on the provided image and outputs the results to standard output.");
+    parser.about("\nThis program detects faces on the provided image or list of images and outputs the results to standard output.\n");
 
     // If help argument is passed, print help and exit
     if (parser.has("help"))
@@ -30,9 +31,10 @@ int main(int argc, const char **argv)
     }
 
     // Find specified parameters
-    String image_path = samples::findFile(parser.get<String>("@image"));
-    String cascade_path = samples::findFile(parser.get<String>("@cascade"));
-    bool display_flag = parser.has("display");
+    String inputFilePath = parser.get<String>("@inputFilePath");
+    String cascadePath = samples::findFile(parser.get<String>("@cascade"));
+    String imageTopDir = samples::findFile(parser.get<String>("imageTopDir"));
+    bool displayFlag = parser.has("display");
     double scaleFactor = parser.get<double>("scale");
     int minNeighbors = parser.get<uint>("neighbors");
 
@@ -43,49 +45,89 @@ int main(int argc, const char **argv)
         return 1;
     }
 
-    // Load specified cascade and image
-    if (!cascade.load(cascade_path))
+    // Detect input file type
+    vector<String> imageList; // used for outputting correct filenames
+    vector<String> imagePathList;
+
+    if (inputFilePath.length() >= 4 && inputFilePath.substr(inputFilePath.length() - 4) == ".txt")
     {
-        cerr << "Error loading face cascade:" << cascade_path << "\n";
+        // If the file ends in '.txt', parse it and prepare image paths for later processing
+        ifstream inputFile(inputFilePath);
+        if (!inputFile.is_open())
+        {
+            cerr << "Error: Unable to open the file.\n";
+            return 1;
+        }
+
+        String line;
+        String fwdslash("/");
+        String extension(".jpg"); // TODO: This only works with JPGs now
+        while (getline(inputFile, line))
+        {
+            imagePathList.push_back(imageTopDir + fwdslash + line + extension);
+            imageList.push_back(line);
+        }
+
+        inputFile.close();
+    }
+    else
+    {
+        // Else assume the argument is an image file
+        // Assume the provided inputFilePath is an entire path, eg. with file extension
+        String fwdslash("/");
+        imagePathList.push_back(imageTopDir + fwdslash + inputFilePath);
+        imageList.push_back(inputFilePath);
+    }
+
+    // Load specified cascade
+    if (!cascade.load(cascadePath))
+    {
+        cerr << "Error loading face cascade:" << cascadePath << "\n";
         return -1;
     }
 
-    Mat image = imread(image_path, IMREAD_COLOR);
-    if (image.empty())
+    // Process each image in the list
+    for (int i = 0; i < imageList.size(); i++)
     {
-        std::cerr << "Error reading the image: " << image_path << "\n";
-        return 1;
-    }
+        string imagePath = imagePathList.at(i);
 
-    // Perform face detection
-    Mat image_gray;
-    std::vector<Rect> faces;
-    std::vector<int> rejectLevels;
-    std::vector<double> weights;
-    cvtColor(image, image_gray, COLOR_BGR2GRAY);
-    equalizeHist(image_gray, image_gray);
-    // image, objects, rejectLevels, levelWeights, scaleFactor, minNeighbors, flags, minSize, maxSize, outputRejectLevels
-    cascade.detectMultiScale(image_gray, faces, rejectLevels, weights, scaleFactor, minNeighbors, 0, Size(), Size(), true);
- 
-    // Generate output
-    cout << image_path << "\n";
-    cout << faces.size() << "\n";
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        cout << faces[i].x << " " << faces[i].y << " " << faces[i].width << " " << faces[i].height << " " <<  weights[i] << "\n";
-    }
+        Mat image = imread(imagePath, IMREAD_COLOR);
+        if (image.empty())
+        {
+            std::cerr << "Error reading the image: " << imagePath << "\n";
+            return 1;
+        }
 
-    // If requested, display image with detections
-    if (display_flag)
-    {
+        // Perform face detection
+        Mat imageGray;
+        std::vector<Rect> faces;
+        std::vector<int> rejectLevels;
+        std::vector<double> weights;
+        cvtColor(image, imageGray, COLOR_BGR2GRAY);
+        equalizeHist(imageGray, imageGray);
+        // image, objects, rejectLevels, levelWeights, scaleFactor, minNeighbors, flags, minSize, maxSize, outputRejectLevels
+        cascade.detectMultiScale(imageGray, faces, rejectLevels, weights, scaleFactor, minNeighbors, 0, Size(), Size(), true);
+
+        // Generate output
+        cout << imageList.at(i) << "\n";
+        cout << faces.size() << "\n";
         for (size_t i = 0; i < faces.size(); i++)
         {
-            Point p1(faces[i].x, faces[i].y);
-            Point p2(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-            rectangle(image, p1, p2, Scalar(0, 255, 255), 1, LINE_4);
+            cout << faces[i].x << " " << faces[i].y << " " << faces[i].width << " " << faces[i].height << " " << weights[i] << "\n";
         }
-        imshow("Face detection", image);
-        waitKey();
+
+        // If requested, display image with detections
+        if (displayFlag)
+        {
+            for (size_t i = 0; i < faces.size(); i++)
+            {
+                Point p1(faces[i].x, faces[i].y);
+                Point p2(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+                rectangle(image, p1, p2, Scalar(0, 255, 255), 1, LINE_4);
+            }
+            imshow("Face detection", image);
+            waitKey();
+        }
     }
 
     return 0;
